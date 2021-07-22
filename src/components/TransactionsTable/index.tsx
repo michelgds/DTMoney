5d83b5a0fade18c04d15111a701ee218da5e7e1a
Michel G. dos Santos <michel.dossantos@worldpay.com>
2021-07-22 18:50:34 -0300

    Add mirajs library for faking apis

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,6 +3,12 @@ import { Container } from "./styles";
 
 export function TransactionsTable() {
 
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, []);
+
   return (
     <Container>
       <table>
